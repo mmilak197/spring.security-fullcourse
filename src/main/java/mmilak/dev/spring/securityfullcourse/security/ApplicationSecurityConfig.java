116d6c2c3bbe44fc2efe3c25e1ff2465831797a2
mzma <mzma@gft.com>
2020-09-10 09:27:46 +0200

    change defaul login page to customize login page

--- src/main/java/mmilak/dev/spring/securityfullcourse/security/ApplicationSecurityConfig.java
+++ src/main/java/mmilak/dev/spring/securityfullcourse/security/ApplicationSecurityConfig.java
@@ -74,6 +74,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 }
