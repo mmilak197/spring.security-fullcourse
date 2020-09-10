869bccf3c5c16dd47e28ec8901250cfb7a2ddad0
mzma <mzma@gft.com>
2020-09-10 08:46:04 +0200

    Control permission use adnotation @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_STUDENT')" and @EnableGlobalMethodSecurity without use configure(HttpSecurity http) method

--- src/main/java/mmilak/dev/spring/securityfullcourse/student/StudentManagementController.java
+++ src/main/java/mmilak/dev/spring/securityfullcourse/student/StudentManagementController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("management/api/v1/students")
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class StudentManagementController {
 
     private static List<Student> STUDENTS = Arrays.asList(
@@ -21,28 +21,28 @@ public class StudentManagementController {
     // hasRole('ROLE_')    hasAnyRole('ROLE_')     hasAuthority('permission')   hasAnyAuthority('permission')
 
     @GetMapping
-//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         System.out.println("getAllStudents");
         return STUDENTS;
     }
 
     @PostMapping
-//    @PreAuthorize("hasAuthority('student:write')")
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
-//    @PreAuthorize("hasAuthority('student:write')")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
-//    @PreAuthorize("hasAuthority('student:write')")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println("updateStudent");
         System.out.println(String.format("%s %s", studentId, student));
