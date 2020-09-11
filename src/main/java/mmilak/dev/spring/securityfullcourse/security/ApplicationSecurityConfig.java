26c978eeb6a5d862821d3af103364c5a2788927c
mzma <mzma@gft.com>
2020-09-11 09:05:13 +0200

    Verify Jwt Token and pass to next filer / api -> filterChain.doFilter(request, response)

--- src/main/java/mmilak/dev/spring/securityfullcourse/security/ApplicationSecurityConfig.java
+++ src/main/java/mmilak/dev/spring/securityfullcourse/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package mmilak.dev.spring.securityfullcourse.security;
 
 import mmilak.dev.spring.securityfullcourse.auth.ApplicationUserService;
+import mmilak.dev.spring.securityfullcourse.jwt.JwtTokenVerifier;
 import mmilak.dev.spring.securityfullcourse.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -39,6 +40,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
